1d0051ef86335875a63f326e45a1f8720a8272a2
ethanaeljford <ethanaeljford@tamu.edu>
2026-09-07 18:45:45 -0500

    Test 2 -- Author, Price, Published Date

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -17,9 +17,26 @@ RSpec.describe Book, type: :model do
       expect(book).not_to be_valid
     end
 
+    it "is not valid without an author" do 
+        book = Book.new(title: "The Fountainhead", author: nil)
+        expect(book).not_to be_valid
+    end
+    
+    it "is not valid with a negative price value" do   
+        book = Book.new(title: "The Fountainhead", author: "Ayn Rand", price: -10.00)
+            expect(book).not_to be_valid
+        end
+    
+    it "is not valid without a published date" do
+        book = Book.new(title: "The Fountainhead", author: "Ayn Rand", price: 19.99, published_date: nil)
+        expect(book).not_to be_valid
+    end
+
+    
+
   end
   describe "attributes" do
-    it "stores aithor, price, and published_date" do
+    it "stores author, price, and published_date" do
         book = Book.new(title: "The Fountainhead", author: "Ayn Rand", price: 19.99, published_date: Date.new(1943, 5, 1))
         expect(book.author).to eq("Ayn Rand")
         expect(book.price).to eq(19.99)
