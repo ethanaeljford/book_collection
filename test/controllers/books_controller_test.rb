694428fac340cfefb23baa25341797fbd4e6ce8d
ethanaeljford <ethanaeljford@tamu.edu>
2026-09-09 11:38:45 -0500

    Heroku 3

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -17,7 +17,11 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
 
   test "should create book" do
     assert_difference("Book.count") do
-      post books_url, params: { book: { title: @book.title } }
+      post books_url, params: { book: { 
+        title: @book.title,
+        author: @book.author,
+        price: @book.price,
+        published_date: @book.published_date } }
     end
 
     assert_redirected_to book_url(Book.last)
